226135a0604ed66578c604d4a7ab8f7e00f85f37
Gabriel Vigliensoni <gabriel@vigliensoni.com>
2018-12-21 11:06:31 -0500

    Fix: Implement schema validation.
    The schema validation implemented includes the data from the submitter 
    and the name of the victim. To be determined what other fields will be 
    needed. Close #7.

--- mongo_conf/database_schema.js
+++ mongo_conf/database_schema.js
@@ -4,15 +4,31 @@ db.runCommand( {
    collMod: 'victimes',
    validator: { $jsonSchema: {
       bsonType: 'object',
-      required: [ 'Prénom de la personne que soumet le formulaire'
-                  // 'Nom du la personne que soumet le formulaire',
-                  // 'Email address soumeter',
-                  // 'Relation avec la victime',
-                  // 'Prénom de la victime',
-                  // 'Nom de la victime'
+      required: [ 'Prénom de la personne que soumet le formulaire',
+                  'Nom de la personne que soumet le formulaire',
+                  'Email address soumeter',
+                  'Relation avec la victime',
+                  'Prénom de la victime',
+                  'Nom de la victime'
                 ],
       properties: {
          'Prénom de la personne que soumet le formulaire': {
+            bsonType: 'string',
+            description: 'must be a string and is required'
+         },
+         'Nom de la personne que soumet le formulaire': {
+            bsonType: 'string',
+            description: 'must be a string and is required'
+         },
+         'Email address soumeter': {
+            bsonType: 'string',
+            description: 'must be a string and is required'
+         },
+         'Relation avec la victime': {
+            bsonType: 'string',
+            description: 'must be a string and is required'
+         },
+         'Prénom de la victime': {
             bsonType: 'string',
             description: 'must be a string and is required'
          },
